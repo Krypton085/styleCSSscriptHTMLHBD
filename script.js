541820bbc4de171c3df057edf1c27170fbfc2923
Krypton085 <103414133+Krypton085@users.noreply.github.com>
2022-04-12 15:22:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+async function maubucin(){
+await swals.fire({
+  title: '<strong>Follow Instagram</strong>',
+  html:
+    'Cek scriptnya di ' +
+    '<a href="https://instagram.com/alwiasmart">AlwiASmart</a> ' +
+    '' + '<br><br>' + 'Makaciw:v',
+  focusConfirm: false,
+});} 
