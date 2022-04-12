fc558d3d2e0e02cf48ed08daa71ecb569b26004f
Krypton085 <103414133+Krypton085@users.noreply.github.com>
2022-04-12 15:52:49 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-async function maubucin(){
-await swals.fire({
-  title: '<strong>Follow Dong!!!</strong>',
-  html:
-    'Follow di ' +
-    '<a href="https://instagram.com/alwiasmart">AlwiASmart</a> ' +
-    '' + '<br><br>' + 'Makaciw:v',
-  focusConfirm: false,
-});} 
